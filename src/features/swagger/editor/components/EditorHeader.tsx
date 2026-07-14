c09e3ae4b3e9714d1f77bb6d12a7d8e35d4c5059
AnnaDzig <adzyhota@protonmail.com>
2026-07-14 18:15:42 +0200

    chore: remove console statements

--- src/features/swagger/editor/components/EditorHeader.tsx
+++ src/features/swagger/editor/components/EditorHeader.tsx
@@ -41,7 +41,6 @@ export default function EditorHeader({
   onSchemaSelect,
 }: HeaderProps) {
   const isLandscape = useOrientation();
-  console.log(isValid);
   return (
     <div className="flex flex-wrap items-center gap-3 px-4 py-3 justify-between">
       <div className="rounded-xl border border-slate-800 bg-slate-900 p-1 md:flex">
